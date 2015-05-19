b5281ea10f693bc6245aa892ca13e90082480af3
Alexander Zizzo <elzi@nerdi.net>
2015-05-19 10:36:53 -0700

    level 5 solution

--- Player.js
+++ Player.js
@@ -1,3 +1,4 @@
+// Set health variable to compare in turn to see if we're taking ranged damage
 var _health = undefined;
 
 class Player {
@@ -9,17 +10,24 @@ class Player {
       console.log('ATTACK' )
       _health = warrior.health()
       warrior.attack()
-    } else {
+    } 
+    // If we're at a captive, rescue him
+    else if ( warrior.feel().isCaptive() ) {
+      warrior.rescue()
+    }
+    // Otherwise run attack/health sequence
+    else {
       // If we're taking damage, there's a ranged attacker, walk
       if ( _health > warrior.health() ) {
         console.log( 'TAKING DAMAGE, WALK' )
         warrior.walk()
       } 
       // If we're at a stable health, walk
-      else if ( warrior.health() > 12 ) {
+      else if ( warrior.health() > 19 ) {
         console.log( 'WALK' )
         warrior.walk()
       } 
+
       // Otherwise rest up
       else {
         console.log( 'REST' )
