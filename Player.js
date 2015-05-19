ad86d88d5339c553378eea4a5e5e910dff9f7f95
Alexander Zizzo <elzi@nerdi.net>
2015-05-19 10:33:30 -0700

    level 4 solution

--- Player.js
+++ Player.js
@@ -1,18 +1,33 @@
+var _health = undefined;
+
 class Player {
-  playTurn(warrior) {
-    
+
+  playTurn(warrior) {    
+
+    // If we're next to an enemy
     if ( warrior.feel().isEnemy() ) {
+      console.log('ATTACK' )
+      _health = warrior.health()
       warrior.attack()
     } else {
-      if ( warrior.health() > 10 ) {
+      // If we're taking damage, there's a ranged attacker, walk
+      if ( _health > warrior.health() ) {
+        console.log( 'TAKING DAMAGE, WALK' )
         warrior.walk()
-      } else {
+      } 
+      // If we're at a stable health, walk
+      else if ( warrior.health() > 12 ) {
+        console.log( 'WALK' )
+        warrior.walk()
+      } 
+      // Otherwise rest up
+      else {
+        console.log( 'REST' )
         warrior.rest()
       }
     }
-  
-
   }
 }
+
 
 global.Player = Player;
